59a34f3d8fd4068d83f5a034e098100f7417d7d7
Unknown <thylle@kruso.dk>
2018-04-28 09:43:51 +0200

    Dialog is now a component

---
+++ components/AddExerciseDialog.vue
@@ -0,0 +1,86 @@
+<template>
+  <el-dialog title="Your new exercise" width="90%" v-bind:visible.sync="addExerciseValues.active">
+    <div class="AddExerciseDialog">
+      <form v-on:submit.prevent="addExercise()">
+          <input class="form-control  mb-2" placeholder="Name your exercise" v-model="newExercise.name" />
+          <textarea class="form-control  mb-3" placeholder="Make a note to the exercise" v-model="newExercise.notes" />
+          
+          <!-- Option list of workouts -->
+          <div>
+            <label>Choose workout </label>
+            <button class="float-right link" type="button" tabindex="-1" v-on:click="addWorkoutActive = true">Create new workout</button>
+          </div>
+          
+          <select class="form-control  mb-3" v-model="addExerciseValues.workoutKey">
+              <option
+              v-for="item in workouts"
+              v-if="item.name"
+              :key="item.key"
+              :label="item.name"
+              :value="item.key">
+              </option>
+          </select>
+
+          <label>Weight in kg</label>
+          <input class="form-control  mb-3" placeholder="How much weight" type="number" v-model="newExercise.weight" v-bind:min="0" />
+              
+          <label>Sets</label>
+          <input class="form-control  mb-3" placeholder="How many sets" type="number" v-model="newExercise.sets" v-bind:min="0" />
+
+          <label>Reps</label>
+          <input class="form-control  mb-3" placeholder="Number of reps" type="number" v-model="newExercise.reps" v-bind:min="0" />
+
+          <label>Rest in sec</label>
+          <input class="form-control  mb-3" placeholder="Rest in seconds" type="number" v-model="newExercise.rest" v-bind:min="0" />
+
+          <div class="text-right  dialog-footer">
+            <button class="btn mr-3" type="button" @click="closeDialog()">Cancel</button>
+            <button type="submit" class="btn btn-success">Create exercise</button>
+          </div>
+      </form>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import APIService from "~/services/api-service.js";
+const API = new APIService();
+
+export default {
+  data() {
+    return {
+      newWorkoutName: null,
+      addWorkoutActive: false,
+      newExercise: {}
+    };
+  },
+
+  props: ["addExerciseValues"],
+
+  computed: {
+    currentUser() {
+      return this.$store.getters.currentUser;
+    },
+    workouts() {
+      return this.$store.getters.workoutsSorted;
+    }
+  },
+
+  methods: {
+    closeDialog(){
+      this.addExerciseValues.active = false;
+    },
+    addExercise() {
+      this.newExercise.workoutKey = this.addExerciseValues.workoutKey;
+
+      API.addExercise(this.currentUser, this.newExercise).then((newItem) => {
+        this.newExercise = {};
+        this.closeDialog()
+
+        //TODO: add new item to store or get data from firebase again...
+        API.getCurrentUserData(this.$store);
+      });
+    }
+  }
+};
+</script>
